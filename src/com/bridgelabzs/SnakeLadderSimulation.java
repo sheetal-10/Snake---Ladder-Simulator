1a0bfe6c9e9b26101b523ace638a251601f318b7
sheetal-10 <sheetalp257@gmail.com>
2021-08-06 07:52:18 +0530

    Added: welcome message

---
+++ src/com/bridgelabzs/SnakeLadderSimulation.java
@@ -0,0 +1,7 @@
+package com.bridgelabzs;
+
+public class SnakeLadderSimulation {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake and Ladder Simulation Program ");
+	}
+}
