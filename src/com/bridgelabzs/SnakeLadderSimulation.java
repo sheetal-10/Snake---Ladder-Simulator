15c3bf812946ec6b3bbbb48b0be244175dc32bc2
sheetal-10 <sheetalp257@gmail.com>
2021-08-07 19:39:31 +0530

    Added: code for player checks for an option

--- src/com/bridgelabzs/SnakeLadderSimulation.java
+++ src/com/bridgelabzs/SnakeLadderSimulation.java
@@ -7,8 +7,8 @@ package com.bridgelabzs;
  */
 public class SnakeLadderSimulation {
 	/**
-	 * creating die roll formula
-	 * returning random no between 1 to 6 
+	 * creating die roll formula and 
+	 * checking for option after die outcome
 	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Simulation Program ");
@@ -17,5 +17,21 @@ public class SnakeLadderSimulation {
 		//die roll giving random no between 1 to 6
 		int die = (int) (Math.floor(Math.random() * 10) % 6 + 1);
 		System.out.println("\nDice roll: " + die);
+		//checking for option of no play, ladder or snake after die roll
+		int option = (int) (Math.floor(Math.random() * 10) % 3);
+		switch (option) {
+			case 1:
+				System.out.println("No play");
+				player_position += 0;
+				break;
+			case 2:
+				System.out.println("Ladder");
+				player_position += die;
+				break;
+			default:
+				System.out.println("Snake");
+				player_position -= die;		
+		}
+		System.out.println("Player position: " + player_position);
 	}
 }
