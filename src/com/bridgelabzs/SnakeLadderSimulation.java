d997b0162d67b5865c552261d6ee4c0b9b0ce46a
sheetal-10 <sheetalp257@gmail.com>
2021-08-07 19:26:37 +0530

    Added: code for player starting position as 0

--- src/com/bridgelabzs/SnakeLadderSimulation.java
+++ src/com/bridgelabzs/SnakeLadderSimulation.java
@@ -1,7 +1,18 @@
 package com.bridgelabzs;
 
+/**
+ * This problem simulates a Snake and Ladder game
+ * @author Sheetal
+ * @since 2021-08-07
+ */
 public class SnakeLadderSimulation {
+	/**
+	 * creating start position zero for the palyer
+	 * returning player position 
+	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Simulation Program ");
+		int player_position = 0;
+		System.out.println("\nStarting position of player is "  + player_position );
 	}
 }
