92ba505cc9954f1a2ff4d92691e5ef541b7d8a7f
sheetal-10 <sheetalp257@gmail.com>
2021-08-07 19:49:34 +0530

    Added: code for iteration to reach winning position

--- src/com/bridgelabzs/SnakeLadderSimulation.java
+++ src/com/bridgelabzs/SnakeLadderSimulation.java
@@ -9,28 +9,37 @@ public class SnakeLadderSimulation {
 	/**
 	 * creating die roll formula and 
 	 * checking for option after die outcome
+	 * iterating till winning position
 	 */
+	
+	public static final int WINNING_POSITION = 100;
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Simulation Program ");
+		
 		//initialising player position
 		int player_position = 0;
+		
+		while ( WINNING_POSITION > player_position ) {
 		//die roll giving random no between 1 to 6
-		int die = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-		System.out.println("\nDice roll: " + die);
+			int die = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+		
 		//checking for option of no play, ladder or snake after die roll
-		int option = (int) (Math.floor(Math.random() * 10) % 3);
-		switch (option) {
+			int option = (int) (Math.floor(Math.random() * 10) % 3);
+			switch (option) {
 			case 1:
-				System.out.println("No play");
+				System.out.println("No play:" +0);
 				player_position += 0;
 				break;
 			case 2:
-				System.out.println("Ladder");
+				System.out.println("Ladder:" + "+" + die );
 				player_position += die;
 				break;
 			default:
-				System.out.println("Snake");
-				player_position -= die;		
+				System.out.println("Snake:" + "-" + die);
+				player_position -= die;	
+				if (player_position < 0)
+					player_position = 0;		
+			}
 		}
 		System.out.println("Player position: " + player_position);
 	}
